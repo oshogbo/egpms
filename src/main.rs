af61e9001c184eb60f92f5946b7afdbf28b74bf0
Mariusz Zaborski <oshogbo@vexillium.org>
2023-03-07 16:57:29 +0100

    Rewrite args parsing

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,6 @@ use std::time::Duration;
 
 const TIMEOUT : Duration = Duration::from_secs(1);
 
-struct Config {
-    cmd: String,
-    socket_id: u8,
-}
-
-impl Config {
-    fn build() -> Result<Config, &'static str> {
-        let args: Vec<String> = env::args().collect();
-
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let socket_id : u8 = match args[2].parse() {
-            Ok(num) => num,
-            Err(_) => return Err("Socket id has to be number"),
-        };
-        if socket_id < 1 || socket_id > 4 {
-            return Err("Unknwon socket");
-        }
-
-        Ok(Config{ cmd: args[1].clone(), socket_id })
-    }
-}
-
-fn usage() -> ! {
-    let args: Vec<String> = env::args().collect();
-
-    println!("Usage: ");
-    println!("{} [status,start,stop] [socket_id]", args[0]);
-
-    process::exit(1);
-}
-
 fn from_id_to_device(id: u8) -> u8 {
     0x03 * id
 }
@@ -64,30 +31,111 @@ impl SetStatus {
     }
 }
 
-fn cmd_start(device: &libusb::DeviceHandle, socket_id: u8) {
-    SetStatus::StatusStart.exec(device, socket_id);
+fn parse_socket_id(args: &[String]) -> Result<u8, &'static str> {
+    if args.len() < 1 {
+        return Err("not enough arguments");
+    }
+    let socket_id : u8 = match args[0].parse() {
+        Ok(num) => num,
+        Err(_) => return Err("Socket id has to be number"),
+    };
+    if socket_id < 1 || socket_id > 4 {
+        return Err("Unknwon socket");
+    }
+
+    Ok(socket_id)
 }
 
-fn cmd_stop(device: &libusb::DeviceHandle, socket_id: u8) {
-    SetStatus::StatusStop.exec(device, socket_id);
+trait ConfigCMD {
+    fn run(&self, device: &libusb::DeviceHandle);
+    fn parse(args: &[String]) -> Result<Box<dyn ConfigCMD>, &'static str> where Self: Sized;
 }
 
-fn cmd_status(device: &libusb::DeviceHandle, socket_id: u8) {
-    let mut data : [u8; 2] = [0x02, 0x00];
-    let devid = from_id_to_device(socket_id);
-    device.read_control(
-        0xa1,
-        0x01,
-        0x0300 + (devid as u16),
-        0x0,
-        &mut data,
-        TIMEOUT,
-    ).unwrap();
-    println!("Socket {} is {}", socket_id, if data[1] == 0 { "offline" } else { "online" } );
+struct ConfigStart {
+    socket_id: u8,
+}
+
+struct ConfigStop {
+    socket_id: u8,
+}
+
+struct ConfigStatus {
+    socket_id: u8,
+}
+
+impl ConfigCMD for ConfigStart {
+    fn parse(args: &[String]) -> Result<Box<dyn ConfigCMD>, &'static str> {
+        match parse_socket_id(args) {
+            Ok(socket_id) => Ok(Box::new(Self{socket_id})),
+            Err(err) => Err(err),
+        }
+    }
+
+    fn run(&self, device: &libusb::DeviceHandle) {
+        SetStatus::StatusStart.exec(device, self.socket_id);
+    }
+}
+
+impl ConfigCMD for ConfigStop {
+    fn parse(args: &[String]) -> Result<Box<dyn ConfigCMD>, &'static str> {
+        match parse_socket_id(args) {
+            Ok(socket_id) => Ok(Box::new(Self{socket_id})),
+            Err(err) => Err(err),
+        }
+    }
+    fn run(&self, device: &libusb::DeviceHandle) {
+        SetStatus::StatusStop.exec(device, self.socket_id);
+    }
+}
+
+impl ConfigCMD for ConfigStatus {
+    fn parse(args: &[String]) -> Result<Box<dyn ConfigCMD>, &'static str> {
+        match parse_socket_id(args) {
+            Ok(socket_id) => Ok(Box::new(Self{socket_id})),
+            Err(err) => Err(err),
+        }
+    }
+    fn run(&self, device: &libusb::DeviceHandle) {
+        let mut data : [u8; 2] = [0x02, 0x00];
+        let devid = from_id_to_device(self.socket_id);
+        device.read_control(
+            0xa1,
+            0x01,
+            0x0300 + (devid as u16),
+            0x0,
+            &mut data,
+            TIMEOUT,
+        ).unwrap();
+        println!("Socket {} is {}", self.socket_id,
+            if data[1] == 0 { "offline" } else { "online" }
+        );
+    }
+}
+
+fn parse_cmd() -> Result<Box<dyn ConfigCMD>, &'static str> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return Err("Missing cmd");
+    }
+    match args[1].as_str() {
+        "start" => ConfigStart::parse(&args[2..]),
+        "stop" => ConfigStop::parse(&args[2..]),
+        "status" => ConfigStatus::parse(&args[2..]),
+        _ => Err("Unknwon option"),
+    }
+}
+
+fn usage() -> ! {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Usage: ");
+    println!("{} [status,start,stop] [socket_id]", args[0]);
+
+    process::exit(1);
 }
 
 fn main() {
-    let config = Config::build().unwrap_or_else(|err| {
+    let cmd = parse_cmd().unwrap_or_else(|err| {
         println!("Error {}", err);
         println!("");
         usage();
@@ -96,13 +144,5 @@ fn main() {
     let context = libusb::Context::new().unwrap();
     let device = context.open_device_with_vid_pid(0x04b4, 0xfd15).unwrap();
 
-    match config.cmd.as_str() {
-        "start" => cmd_start(&device, config.socket_id),
-        "stop" => cmd_stop(&device, config.socket_id),
-        "status" => cmd_status(&device, config.socket_id),
-        _ => {
-            println!("Unknwon option");
-            usage();
-        },
-    }
+    cmd.run(&device);
 }
